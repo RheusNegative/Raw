240f5eb9be2e2b32374ed450d1ef1253c0f364f7
Tango-Wulf <tangoswulf@protonmail.ch>
2018-04-23 20:40:47 -0700

    url-loader file-loader

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import image from '../assets/images/gridResult.png'
 
 const App = (props) => {
   return (
     <div className="container-fluid">
-      
+      <div className="row"><img src={image} alt="none"/></div>
     </div>
   )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,12 @@ module.exports = {
     filename: 'bundle.js'
   },
   module: {
-  rules: [
-    { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+      {test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,loader: 'url-loader',
+      options: { limit: 10000
+      }
+    }
   ]
 }
 };
